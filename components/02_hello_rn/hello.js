1ae2a519e1e7f094602cbe773705440daf0374a8
tianyu5177 <zhangtianyu5177@sina.com>
2020-08-25 10:52:38 +0800

    更新

---
+++ components/02_hello_rn/hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Text} from 'react-native'
+
+export default class App extends Component {
+	render() {
+		return (
+			<Text style={{fontSize:20}}>
+				<h1>Hello,RN!</h1>
+			</Text>
+		)
+	}
+}
